ecd38c546f12d2ceafa97e41147549323754f6fa
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-11-14 16:04:29 -0500

    OOP changes

--- main/pdf2txt.py
+++ main/pdf2txt.py
@@ -1,18 +1,15 @@
 import os
 from tika import parser  # https://pypi.org/project/tika/
 from typing import List, Any
+from sys import argv
 
 # Even though "read_pdf" should retun a ByteString, bytes or Buffers, there's no support for those
 # kinds of types/values yet... So I would use 'Any'
 
 
 class pdf2text():
-    def __init__(self, input_path: str, output_path: str) -> None:
-        self.input_path = input_path
-        self.output_path = output_path
-        return None
 
-    def read_pdf(pdf_file: str) -> Any:
+    def read_pdf(self, pdf_file: str) -> Any:
         """
             read content from pdf files and return it encoded in utf-8
             to avoid special characters problems
@@ -20,7 +17,7 @@ class pdf2text():
         text = parser.from_file(pdf_file)['content']
         return text.encode('utf-8')
 
-    def pdf_to_txt(pdf_folder_path: str, txt_folder_path: str) -> None:
+    def pdf_to_txt(self, pdf_folder_path: str, txt_folder_path: str) -> None:
         """
         pdf_folder_path: path to your pdf's
         txt_folder_path: path where you want .txt files saved
@@ -37,7 +34,7 @@ class pdf2text():
         for pdf_ in pdf_files:
             text_file: str = os.path.splitext(os.path.basename(pdf_))[0]+'.txt'
             with open(os.path.join(txt_folder_path, text_file), 'wb') as text_in_file:
-                text_in_file.write(read_pdf(pdf_))
+                text_in_file.write(self.read_pdf(pdf_))
 
         return None
 
@@ -45,13 +42,18 @@ class pdf2text():
 def main() -> None:
     """
         run the script
-    """
-    pdf_path: str = r"C:\Users\Eliecer\PRISM\text-mining-prism-data\text-mining-prism\estudios"
-    txt_path: str = r"C:\Users\Eliecer\PRISM\txt"
-    pdf_info = pdf2text(r"text-mining-prism\estudios",
-                        r"C:\Users\Eliecer\PRISM\txt")
+        path where the PDF's files are
+        path for the result folder containing the data
 
-    pdf_to_txt(pdf_path, txt_path)
+    """
+    try:
+        pdf_path: str = argv[1]
+        txt_path: str = argv[2]
+        pdf_info = pdf2text()
+        pdf_info.pdf_to_txt(pdf_path, txt_path)
+
+    except Exception as error_info:
+        print(error_info.args)
 
     return None
 
