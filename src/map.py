48741804c15f3cbf1fcc8e676ecc357b241b1a7e
Eliecer <eliecer.aguilar1@utp.ac.pa>
2022-12-22 10:55:30 -0500

    graficar con matplotlib

--- src/map.py
+++ src/map.py
@@ -1,45 +1,30 @@
 import pandas as pd
 import numpy as np
 import geopandas as gpd
-from shapely.geometry import Point
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 import toml
 import matplotlib.pyplot as plt
-from shapely.errors import ShapelyDeprecationWarning
-import warnings
-
-# warning de shapely tipo de datos que ya no se usaran
-warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
+import plotly.express as px
 
 try:
-    table01 = pd.read_csv(
-        r"..\csv\00-tabla-hogares-Encuesta de Niveles de Vida 2003.csv")
+    pdf_table01 = pd.read_csv(r"..\csv\00-tabla-hogares-Encuesta de Niveles de Vida 2003.csv")
+    pdf_map_cord = pd.read_csv(r"..\pan_coord.csv")
+    pdf_merge = pdf_table01.merge(pdf_map_cord, left_on="lugar", right_on="Province", how="left")
+    pdf_merge.drop(labels="Province", axis=1, inplace=True)
 
-    pdf_table01 = pd.DataFrame(data=table01)
+    # Convert the "lat" and "long" columns to geometry points
+    # pdf_merge["geometry"] = pdf_merge.apply(lambda row: row if np.isnan(row["lat"]) else gpd.points_from_xy(row["long"], row["lat"]), axis=1)
+    # pdf_merge["geometry"] = pdf_merge.apply(
+    #     lambda row: gpd.points_from_xy(row["long"], row["lat"]) if not pd.notnull(row["lat"]) and not pd.notnull(row["long"]) else np.nan,
+    #     axis=1
+    # )
 
-    pan_data = pd.read_csv(r"..\pan_coord.csv")
-    pdf_map_cord = pd.DataFrame(data=pan_data)
-
-    values = list(pdf_map_cord.Province)
-    index = []
-
-    for x in values:
-        index.append([int(pdf_table01.index[pdf_table01['lugar'] == x].to_list()[0]),
-                      int(pdf_map_cord.index[pdf_map_cord['Province'] == x].to_list()[0]), x])
-
-    pdf_table01["lat"] = np.nan
-    pdf_table01["long"] = np.nan
-
-    for idx, val in enumerate(index):
-        pdf_table01.at[val[0], 'lat'] = pdf_map_cord.at[val[1], 'lat']
-        pdf_table01.at[val[0], 'long'] = pdf_map_cord.at[val[1], 'long']
-
-    # Convert the "lat" and "lon" columns to geometry points
-    pdf_table01["geometry"] = pdf_table01.apply(lambda row: Point(row["long"], row["lat"]), axis=1)
-    pdf_table01.to_csv("tabla01.csv")
+    geometry = gpd.points_from_xy(pdf_merge["lat"], pdf_merge["long"])
+    pdf_merge["geometry"] = geometry
+    # pdf_merge.to_csv("tabla01.csv")
     # Convert the dataframe to a GeoDataFrame
-    gdf = gpd.GeoDataFrame(pdf_table01, geometry=pdf_table01["geometry"], crs="epsg:4326")
+    gdf = gpd.GeoDataFrame(pdf_merge, geometry="geometry", crs="epsg:4326")
 
     config = toml.load("conf.toml")
     pasw: str = config["dbms_conf"]["_pas"]
@@ -54,18 +39,54 @@ try:
 
     sql = """select * from pan_map;"""
     gdf_map = gpd.read_postgis(sql=sql, con=engine, geom_col="geom", crs='epsg:4326')
-    gdf_info = gdf.iloc[16:, 0:-3]
 
+    pdf_info = pdf_table01.merge(pdf_map_cord, left_on="lugar", right_on="Province")
+    geom = gpd.points_from_xy(pdf_info["long"], pdf_info["lat"])
+    pdf_info["geometry"] = geom
+    pdf_info = pdf_info.merge(gdf_map, left_on="lugar", right_on="nombre")
+    pdf_info.drop(labels=["geom","nombre","tipo", "Province"], inplace=True, axis=1)
+
+    gdf_info = gpd.GeoDataFrame(data=pdf_info,geometry="geometry",crs=4326)
     # graficar un mapa sobre el otro
-    fig, ax = plt.subplots(figsize=(10, 10))
-    gdf_map = gdf_map.to_crs(epsg=4326)
-    gdf_map.plot(cmap="magma", edgecolor='black', column='name_1', ax=ax)
-    gdf = gdf.to_crs(epsg=4326)
-    gdf.plot(ax=ax, cmap='viridis', edgecolor='black')
-    plt.show()
+    # fig, ax = plt.subplots(figsize=(10, 10))
+    # gdf_map.plot(cmap="magma", column='nombre', linewidth=2, facecolor="black", ax=ax)
+    # gdf_info.plot(ax=ax, cmap='viridis')
+
+    #plotly
+    # pan, ax = plt.subplots(figsize=(10,10),subplot_kw=dict(aspect="equal"))
+
+    # pan = px.choropleth(pdf_info,
+    #                     locations="pk",
+    #                     geojson=gdf_map,
+    #                     color="Muestra_Expandida",
+    #                     title="Encuesta niveles de vida",
+    #                     color_continuous_scale=px.colors.sequential.Plasma,
+    #                     hover_name="lugar",
+    #                     hover_data=["Muestra1"],
+    #                     labels={"datos de muestra": "porcentaje"},
+    #                     scope="north america")
+
+    pan = px.choropleth_mapbox(
+                    pdf_info,
+                    locations="pk",
+                    geojson=gdf_map,
+                    color="Muestra1",
+                    color_continuous_scale=px.colors.sequential.Plasma,
+                    title="Encuesta de niveles de vida, 2003",
+                    labels={"datos de muestra": "porcentaje"},
+                    hover_name="lugar",
+                    hover_data=["Muestra1","Muestra2"],
+                    mapbox_style="open-street-map",
+                    zoom=3,
+                    opacity=0.5,
+                    center={'lat': 8.5380, 'lon': -80.7821},
+                    )
+    pan.update_geos(fitbounds="locations",visible=False)
+    pan.show()
+    # fig.show()
     print("success")
 
 
 except Exception as e:
-    print(e.args)
+    print(f"el error esta en : {e}")
 
